e6844a85e5cc7045db3e66a486865326f9e94358
Kyle <kylemewing@gmail.com>
2019-10-25 21:46:00 -0700

    Initial Commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MoonCalculator.Models;
+
+namespace MoonCalculator.Controllers
+{
+    [Route("")]
+    public class HomeController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("submit")]
+        public IActionResult ScanSubmit(string raw_input) {
+            Console.WriteLine(raw_input);
+            return Redirect("moon-results");
+        }
+
+        [HttpGet("moon-results")]
+        public IActionResult MoonResults(Dictionary<string, Moon> results) {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Moon.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+using System;
+using System.Collections.Generic;
+
+namespace MoonCalculator.Models {
+    public class Moon {
+        public Dictionary<string, string> oreInfo {get;set;}
+
+    }
+}
